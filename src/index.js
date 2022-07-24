ee79e970d3e43d3d7fd0f19c52ccdde33b6f7278
Samuel OSita <samearusuosita.com>
2022-07-24 09:16:25 +0100

    edit router

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { Footer, Navbar } from './components';
 
 
 import {
-  BrowserRouter,
   Routes,
   Route,
+  HashRouter,
 } from "react-router-dom";
 
 
@@ -25,7 +25,7 @@ root.render(
   
   <React.StrictMode>
     <Navbar />
-      <BrowserRouter>
+      <HashRouter>
       
         <Routes>
           <Route path="/" element={<App />}/>
@@ -43,12 +43,9 @@ root.render(
           />
         </Routes>
     
-      </BrowserRouter>
+      </HashRouter>
       <Footer />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
